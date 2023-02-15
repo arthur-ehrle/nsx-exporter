92b30f1fc43e0b98d0489edd085ef97689f43730
aehr <109029449+arthur-ehrle@users.noreply.github.com>
2023-02-15 11:11:31 +0100

    Add files via upload

---
+++ api.py
@@ -0,0 +1,42 @@
+# importing the requests library
+import requests
+import re
+from requests.auth import HTTPBasicAuth
+from urllib3.exceptions import InsecureRequestWarning
+from os import path
+import yaml
+
+# doc for API url : https://developer.vmware.com/apis/1083/nsx-t
+url_list=[]
+url_name = []
+if path.exists('config.yml'):
+    with open('config.yml', 'r') as config_file:
+        try:
+            config = yaml.safe_load(config_file)
+            user = config['user']
+            password = config['password']
+            for URL in config['url_list']:
+                url_list.append(URL['link'])
+                url_name.append(URL['name'])
+        except yaml.YAMLError as error:
+            print(error)
+
+def request_agent(URL):
+    requests.packages.urllib3.disable_warnings(category=InsecureRequestWarning)
+
+    # sending get request and saving the response as response object HTTPBasicAuth(user, password)
+    r = requests.get(url=URL,auth=(user,password),verify=False )
+
+    # extracting data in json format
+    data = r.json()
+
+    return data
+
+def result_count_request():
+    data_list = []
+    for URL in url_list:
+        data = request_agent(URL)
+        print(data)
+        data_list.append(data['result_count'])
+    return data_list, url_list, url_name
+
---
+++ collector.py
@@ -0,0 +1,39 @@
+import time
+import random
+import requests
+from os import path
+import yaml
+from prometheus_client.core import GaugeMetricFamily, REGISTRY, CounterMetricFamily
+from prometheus_client import start_http_server
+import api
+
+totalRandomNumber = 0
+class result_count_request(object):
+    def __init__(self):
+        pass
+    def collect(self):
+        data_list, url_list, url_name = api.result_count_request()
+        for URL in url_list:
+            index = url_list.index(URL)
+            gauge = GaugeMetricFamily(f"{label}_{url_name[index]}", f"{url_name[index]}", labels=[f"{url_name[index]}"])
+            val = int(api.logical_router())
+            gauge.add_metric([f'{url_name[index]}'], data_list[index])
+        yield gauge
+
+
+if __name__ == "__main__":
+    if path.exists('config.yml'):
+        with open('config.yml', 'r') as config_file:
+            try:
+                config = yaml.safe_load(config_file)
+                port = int(config['port'])
+                frequency = config['scrape_frequency']
+                label = config['label']
+            except yaml.YAMLError as error:
+                print(error)
+
+    start_http_server(port)
+    REGISTRY.register(result_count_request())
+    while True: 
+        # period between collection
+        time.sleep(frequency)
