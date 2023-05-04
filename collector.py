c14e4a4cceb1f038ee8f37b49c42696f0c01c229
aehr <109029449+arthur-ehrle@users.noreply.github.com>
2023-05-04 10:33:29 +0200

    EXCEPT CONDITIONS
    Addition of some try/except to improve debugging and find if there is a timeout error

--- collector.py
+++ collector.py
@@ -12,25 +12,31 @@ class result_count_request(object):
         pass
     def collect(self):
         data_list, url_list, url_name = api.result_count_request()
-        for URL in url_list:
-            index = url_list.index(URL)
-            gauge = GaugeMetricFamily(f"{label}_{url_name[index]}", f"{url_name[index]}", labels=[f"{url_name[index]}"])
-            val = int(data_list[index])
-            gauge.add_metric([f'{url_name[index]}'], data_list[index])
-            yield gauge
+        try:
+            for URL in url_list:
+                index = url_list.index(URL)
+                gauge = GaugeMetricFamily(f"{label}_{url_name[index]}", f"{url_name[index]}", labels=[f"{url_name[index]}"])
+                val = int(data_list[index])
+                gauge.add_metric([f'{url_name[index]}'], data_list[index])
+                yield gauge
+        except:
+            print(f"Failed to join the destination or error in the API result \n Result of the request : \n*data_list: {data_list} \n*url_list: {url_list} \n*url_name: {url_name}")
+        
 class match_request(object):
     def __init__(self):
         pass
     def collect(self):
-       renderAggreg=api.connectivity_state()
-       for MatchList,NameInList in renderAggreg:
-            for siteDown in MatchList:
-                index = MatchList.index(siteDown)
-                gauge = GaugeMetricFamily(f"{label}_{NameInList}", f"{label}_{NameInList}", labels=[f"{NameInList}"])
-                val = 1
-                gauge.add_metric([f'{siteDown}'], val)
-                yield gauge
-        
+        renderAggreg=api.connectivity_state()
+        try:
+            for MatchList,NameInList in renderAggreg:
+                for siteDown in MatchList:
+                    index = MatchList.index(siteDown)
+                    gauge = GaugeMetricFamily(f"{label}_{NameInList}", f"{label}_{NameInList}", labels=[f"{NameInList}"])
+                    val = 1
+                    gauge.add_metric([f'{siteDown}'], val)
+                    yield gauge
+        except:
+            print(f"Failed to join the destination or error in the API result \n Result of the request : \n*renderAggreg: {renderAggreg} ")
             
 if __name__ == "__main__":
     if path.exists('config/config.yml'):
@@ -43,8 +49,8 @@ if __name__ == "__main__":
                 label = config['label']
             except yaml.YAMLError as error:
                 print(error)
-    print(f"HTTP server started correctly on port {port}")
     start_http_server(port)
+    print(f"HTTP server started correctly on port {port}")
     REGISTRY.register(result_count_request())
     REGISTRY.register(match_request())
     while True: 
